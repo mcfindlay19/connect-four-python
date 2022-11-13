cfe51e3d341a379320fb20135765975b2e745da8
Matt Findlay <mcfindlay19@gmail.com>
2022-11-12 20:24:45 -0700

    Finished win checking logic. Cleaned up code. Add more comments for clarity

--- connect_four.py
+++ connect_four.py
@@ -26,42 +26,83 @@ class Board:
     # 28    29    30    31    32    33    34
     # 35    36    37    38    39    40    41
     def  __init__(self, width = 7, height = 6):
+        # Width and height have a default. Everything else is tied to the height and width of the board
         self.width = width
         self.height = height
         self.coords = []
+        # Need a list of co-ords for easy checking. This is stored as a list of indices to quickly run through to find if a move is winning
+        # left_to_right is a list of indices of the top row and left most column. This is needed for any diagonal where the x and y axis are positively linked
+        # right_to_left is a list of indices of the top row and right most column. This is needed for any diagonal where the x and y axis are negatively linked
+        self.indicies_to_check_left_to_right = []
+        self.indicies_to_check_right_to_left = []
         for row in range(self.height):
+
             for column in range(self.width):
-                self.coords.append([row, column, self.white])
+                list_item = [row, column, self.white]
+                self.coords.append(list_item)
+
+                # Get all indices of the top row and append them to both diagonal lists
+                if row == 0:
+                    self.indicies_to_check_left_to_right.append(self.coords.index(list_item))
+                    self.indicies_to_check_right_to_left.append(self.coords.index(list_item))
+
+                # Get all indices of the left most column, and if they aren't already addto the list, add them. This is only the case for (0, 0)
+                if column == 0:
+
+                    if self.coords.index(list_item) not in self.indicies_to_check_left_to_right:
+                        self.indicies_to_check_left_to_right.append(self.coords.index(list_item))
+
+                # Get all indices of the right most column, and if they aren't already addto the list, add them. This is only the case for (0, width - 1)
+                if column == self.width-1:
+                    
+                    if self.coords.index(list_item) not in self.indicies_to_check_right_to_left:
+                        self.indicies_to_check_right_to_left.append(self.coords.index(list_item))
+
     
     def __repr__(self):
+        # Simplest way to visualize the class
         return str(self.coords)
 
     def print_board(self):
+        # Runs through the list of coordinates. If the coordinate is the last column, print a new line as well
         for coord in self.coords:
-            if coord[1] is 6:
+
+            if coord[1] is (self.width-1):
                 print(coord[2], end= '')
                 print()
+
             else:
                 print(coord[2], end= '')
 
 
     def get_valid_moves(self):
+        # If the column has the top row available, then you must be able to place at least one toekn in that column
         valid_columns = []
         for i in range(self.width):
+
             if self.coords[i][2] is self.white:
                 valid_columns.append(i)
+
         return valid_columns
         
     def play_move(self, selection, player):
+        # Check to ensure the move is legal, and then find the lowest possible row it can belong, and change the owner of that tile from neutral to a colour
         if selection in self.get_valid_moves():
             coord_to_change = [selection]
+
             for i in range(selection, self.width*self.height, self.width):
+
                 if self.coords[i][2] is self.white:
                     coord_to_change = self.coords[i]
+
             i = self.coords.index(coord_to_change)
             self.coords[i][2] = player
+
+        # If that isn't a valid choice, let the player know
         else:
             print("Please choose a different column")
+        
+        # Check for a win after every move
         if self.check_for_win(player) is True:
             print(player + " is a winner")
     
@@ -73,52 +114,159 @@ class Board:
         # The algorithm is iterate through each row, then iterate through each column in that row, then increase the row we look at
         for i in range(self.height): # 0 to 5
             count = 0
+
             for j in range(self.width): # 0 to 6
-                if self.coords[(i*self.width)+j][2] is player:
+                index = (i * self.width) + j
+
+                if self.coords[index][2] is player:
                     count += 1
+
                     if count == 4:
                         return True
+
                 else:
                     count = 0
-        #Check vertical win
+
+        # Check vertical win
         # Start at the top left of the board, and work down the column. If there is at least 4 in a row, that player wins
         # If no winner, move over a column, and start over
         # Co-ordinate checking here needs to start at index 0, then 7, 14, 21, 28, 35, and then wrap around to 1
         # The algorithm here is to iterate through each column, and then each row in that column, then increase the column we look at
         for i in range(self.width): # 0 to 6
             count = 0
+
             for j in range(self.height): # 0 to 5
-                if self.coords[(j*self.width)+i][2] is player:
+                index = (j * self.width) + i
+
+                if self.coords[index][2] is player:
                     count += 1
+
                     if count == 4:
                         return True
+
                 else:
                     count = 0
-            
+
+        # Check through all the possible diagonal start locations for going left to right.
+        # Each check needs to increase index by 8 i.e. 1 more then board width
+        # If we are checking the last column, or the bottom row, stop increasing index
+        # Indices of the diagonals going left to right for reference of a standard connect four board:
+        # 0  8  16 24 32 40
+        # 1  9  17 25 33 41
+        # 2  10 18 26 34
+        # 3  11 19 27
+        # 7  15 23 31 39
+        # 14 22 30 38
+
+        # Run through list of all possible diagonals starting indices
+        for i in self.indicies_to_check_left_to_right:
+            count = 0
+
+            # Run through each diagonal up to height. This ensures we cover every diagonal, but this could end up in overflow, so we need to make sure the index is within bounds
+            for j in range(self.height):
+                index  = i + (j * (self.width + 1))
+
+                # Ensure the index is in bounds
+                if index <= len(self.coords)-1:
+
+                    # Check to see if the coordinate we are checking is on the bottom row. If it is, check if there is a winner. If there is no winner, stop trying to look as there can't be anything below this row
+                    if self.coords[index][0] is (self.height-1):
+
+                        if self.coords[index][2] is player:
+                            count +=1
+
+                            if count == 4:
+                                return True
+
+                        else:
+                            count = 0
+                            break
+
+                    # Check to see if the coordinate we are checking is on the right most column. If it is, check if there is a winner. If there is no winner, stop trying to look as there can't be anything to the right of this column
+                    if self.coords[index][1] is (self.width-1):
+
+                        if self.coords[index][2] is player:
+                            count +=1
+
+                            if count == 4:
+                                return True
+
+                        else:
+                            count = 0
+                            break
+
+                    # If the coordinate is not the bottom row or right column, just continue checking like normal
+                    else:
+                        if self.coords[index][2] is player:
+                            count +=1
+
+                            if count == 4:
+                                return True
+
+                        else:
+                            count = 0
+
+        # Check through all the possible diagonal start locations for going right to left.
+        # Each check needs to increase index by 6 i.e. 1 less then board width
+        # If we are checking the first column, or the bottom row, stop increasing index
+        # Indices of the diagonals going right to left for reference of a standard connect four board:
+        # 3  9  15 21
+        # 4  10 16 22 28
+        # 5  11 17 23 29 35
+        # 6  12 18 24 30 36
+        # 13 19 25 31 37
+        # 20 26 32 38
+
+        # Run through list of all possible diagonals starting indices
+        for i in self.indicies_to_check_right_to_left:
+            count = 0
+
+            # Run through each diagonal up to height. This ensures we cover every diagonal, but this could end up in overflow, so we need to make sure the index is within bounds
+            for j in range(self.height):
+                index  = i + (j*(self.width-1))
+
+                # Ensure the index is in bounds
+                if index <= len(self.coords)-1:
+
+                    # Check to see if the coordinate we are checking is on the bottom row. If it is, check if there is a winner. If there is no winner, stop trying to look as there can't be anything below this row
+                    if self.coords[index][0] is (self.height-1):
+
+                        if self.coords[index][2] is player:
+                            count +=1
+
+                            if count == 4:
+                                return True
+
+                        else:
+                            count = 0
+                            break
+
+                    # Check to see if the coordinate we are checking is on the left most column. If it is, check if there is a winner. If there is no winner, stop trying to look as there can't be anything to the left of this column
+                    if self.coords[index][1] is (0):
+
+                        if self.coords[index][2] is player:
+                            count +=1
+
+                            if count == 4:
+                                return True
+
+                        else:
+                            count = 0
+                            break
+
+                    # If the coordinate is not the bottom row or right column, just continue checking like normal
+                    else:
+                        if self.coords[index][2] is player:
+                            count +=1
+
+                            if count == 4:
+                                return True
+
+                        else:
+                            count = 0
 
 
 blue = '\U0001F535'
 red = '\U0001F534'
 board = Board()
-board.print_board()
-print()
-
-board.play_move(0, red)
-board.play_move(0, blue)
-board.play_move(0, blue)
-board.play_move(0, red)
-board.play_move(0, red)
-board.play_move(0, red)
-board.play_move(1, blue)
-board.play_move(1, blue)
-board.play_move(1, blue)
-board.play_move(1, red)
-board.play_move(1, red)
-board.play_move(1, red)
-board.play_move(5, blue)
-board.play_move(6, blue)
-board.play_move(5, blue)
-board.play_move(6, blue)
-board.play_move(5, blue)
-board.play_move(6, blue)
 board.print_board()
